bc91c60ccc07f38084126b372e0a13395dac2f4c
tadej <tadej@mail.com>
2019-02-28 21:57:25 +0100

    draw snake function updated and key events

--- app.js
+++ app.js
@@ -2,37 +2,98 @@
 * DOM caching
 */
 const canvas = document.getElementById('console');
+const c = canvas.getContext('2d');
 
 /*
 * game settings
 */
-const gameSettings = {
-	playerX: 10,
-	playerY: 10,
-	appleX: 5,
-	appleY: 5
-};
-
-const snake = () => {
-	drawCanvas();
+const settings = {
+	playerX: 10, // player initial x coordinate
+	playerY: 10, // player initial y coordinate
+	appleX: 5, // apple initital x coordinate
+	appleY: 5, // apple initial y coordinate
+	trail: [], // snake trail
+	tail: 4, // initial snake tail length
+	blockSize: 20, // block size
+	dx: 0, // player x speed
+	dy: 0 // player y speed
 };
 
 /*
-* draw canvas
+* main snake game function
+*/
+function snake() {
+	drawCanvas();
+	setInterval(game, 50);
+}
+
+/*
+* draw canvas - set canvas fill and size
 */
 function drawCanvas() {
-	// set 2D canvas
-	const c = canvas.getContext('2d');
-
-	// set canvas fill color
 	c.fillStyle = 'black';
 	c.fillRect(0, 0, canvas.height, canvas.width);
 }
 
 /*
+* draw snake
+*/
+function drawSnake() {
+	// recalculate player position on key event
+	settings.playerX += settings.dx;
+	settings.playerY += settings.dy;
+
+	// draw snake trail rectangle
+	c.fillStyle = 'green';
+	c.fillRect(
+		settings.playerX * settings.blockSize,
+		settings.playerY * settings.blockSize,
+		settings.blockSize - 2,
+		settings.blockSize - 2
+	);
+}
+
+/*
+* draw apple
+*/
+function drawApple() {}
+
+/*
 * run game
 */
-function game() {}
+function game(settings) {
+	drawSnake(settings);
+}
 
-// event listener - DOM loaded
+/*
+* key events
+*/
+function keyPush(e) {
+	switch (e.keyCode) {
+		// left arrow
+		case 37:
+			settings.dx = -1;
+			settings.dy = 0;
+			console.log(settings);
+			break;
+		// up arrow
+		case 38:
+			settings.dx = 0;
+			settings.dy = -1;
+			break;
+		// right arrow
+		case 39:
+			settings.dx = 1;
+			settings.dy = 0;
+			break;
+		// down arrow
+		case 40:
+			settings.dx = 0;
+			settings.dy = 1;
+			break;
+	}
+}
+
+// listen to events
 document.addEventListener('DOMContentLoaded', snake);
+document.addEventListener('keydown', keyPush);
